5e59284f4ab18c15e8265f5412ee519ceea06841
Raul-Holik <113007938+Raul-Holik@users.noreply.github.com>
2023-05-17 09:30:15 +0200

    Add files via upload

---
+++ ZavrsnaApk/ZavrsnaApk/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace ZavrsnaApk
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.upisNovihPolagacaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ispitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.izlaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Lista instruktora";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(33, 99);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(137, 96);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(33, 223);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Upis";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(257, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Vozila";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(260, 99);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(137, 96);
+            this.richTextBox2.TabIndex = 4;
+            this.richTextBox2.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(260, 223);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Registracija";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.upisNovihPolagacaToolStripMenuItem,
+            this.ispitToolStripMenuItem,
+            this.izlaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // upisNovihPolagacaToolStripMenuItem
+            // 
+            this.upisNovihPolagacaToolStripMenuItem.Name = "upisNovihPolagacaToolStripMenuItem";
+            this.upisNovihPolagacaToolStripMenuItem.Size = new System.Drawing.Size(126, 20);
+            this.upisNovihPolagacaToolStripMenuItem.Text = "Upis novih polagaca";
+            this.upisNovihPolagacaToolStripMenuItem.Click += new System.EventHandler(this.upisNovihPolagacaToolStripMenuItem_Click);
+            // 
+            // ispitToolStripMenuItem
+            // 
+            this.ispitToolStripMenuItem.Name = "ispitToolStripMenuItem";
+            this.ispitToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
+            this.ispitToolStripMenuItem.Text = "Ispit";
+            this.ispitToolStripMenuItem.Click += new System.EventHandler(this.ispitToolStripMenuItem_Click);
+            // 
+            // izlaToolStripMenuItem
+            // 
+            this.izlaToolStripMenuItem.Name = "izlaToolStripMenuItem";
+            this.izlaToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
+            this.izlaToolStripMenuItem.Text = "Izlaz";
+            this.izlaToolStripMenuItem.Click += new System.EventHandler(this.izlaToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Aplikacija";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem upisNovihPolagacaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ispitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem izlaToolStripMenuItem;
+    }
+}
+
---
+++ ZavrsnaApk/ZavrsnaApk/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZavrsnaApk
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void upisNovihPolagacaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormUpis upis = new FormUpis();
+            upis.Show();
+        }
+
+        private void ispitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form ispit = new FormIspit();
+            ispit.Show();
+        }
+
+        private void izlaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var messageBox = MessageBox.Show("Da li ste sigurni da želite zatvoriti aplikaciju ", "Potvrda", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+            if (messageBox == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+    }
+}
